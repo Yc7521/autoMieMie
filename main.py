8861516da89b358938f33fac31640bfddc407518
谢永诚 <752106133@qq.com>
2022-09-20 17:17:21 +0800

    update

--- main.py
+++ main.py
@@ -1,68 +1,92 @@
 import argparse
 import json
+from re import T
 import time
 import httpx
+import config
 
-def old_api(header, args):
+http = httpx.Client(proxies={
+    'http://': 'http://127.0.0.1:8888',
+    'https://': 'http://127.0.0.1:8888'
+}, verify=False)
+
+
+def api_get(url: str, header={}, log: bool = True):
     err_time = 0
     err_msg = ""
     while err_time < 5:
         try:
-            response = httpx.get(
-                f"https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score={args.rank_score}&rank_state={args.rank_state}&rank_time={args.rank_time}&rank_role={args.rank_role}&skin={args.skin}&t={args.t}", headers=header).json()
-            print(
-                f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: {response}")
+            response = http.get(url, headers=header).json()
+            if log:
+                print(
+                    f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: {response}")
+            return response
+        except Exception as e:
+            err_time += 1
+            err_msg += f"err-{err_time}: {e}\n"
+            continue
+    if log:
+        print(
+            f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: 请求失败")
+    raise Exception(f"请求失败:\n{err_msg}")
+
+
+def old_api(url="/sheep/v1/game/game_over", header={}, args={}):
+    api_get(
+        f"https://cat-match.easygame2021.com{url}?rank_score={args.rank_score}&rank_state={args.rank_state}&rank_time={args.rank_time}&rank_role={args.rank_role}&skin={args.skin}&t={args.t}",
+        header)
+
+
+def temp_api(header, args, log=True):
+    err_time = 0
+    err_msg = ""
+    while err_time < 5:
+        try:
+            response = http.post(
+                f"https://cat-match.easygame2021.com/sheep/v1/game/game_over_ex",
+                headers=header,
+                content=json.dumps({
+                    "rank_score": args.rank_score,
+                    "rank_state": args.rank_state,
+                    "rank_time": args.rank_time,
+                    "rank_role": args.rank_role,
+                    "skin": args.skin,
+                    "MatchPlayInfo": config.steps
+                })
+            ).json()
+            if log:
+                print(
+                    f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: {response}")
             return
         except Exception as e:
             err_time += 1
             err_msg += f"err-{err_time}: {e}\n"
             continue
-    print(f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: 请求失败")
+    if log:
+        print(
+            f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: 请求失败")
     raise Exception(f"请求失败:\n{err_msg}")
 
-def temp_api(header, args):
-    err_time = 0
-    err_msg = ""
-    while err_time < 5:
-        try:
-            response = httpx.post(
-                 f"https://cat-match.easygame2021.com/sheep/v1/game/game_over_ex", headers=header,
-                 # rank_score={args.rank_score}&rank_state={args.rank_state}&rank_time={args.rank_time}&rank_role={args.rank_role}&skin={args.skin}&t={args.t}
-                 content=json.dumps({
-                     "rank_score": args.rank_score,
-                     "rank_state": args.rank_state,
-                     "rank_time": args.rank_time,
-                     "rank_role": args.rank_role,
-                     "skin": args.skin,
-                     "MatchPlayInfo": "CAMiBQjnARAAIgYI5gEQ2wEiBgiAAhDWASIGCP8BEN8BIgYInwIQsAQiBgieAhCQAyIGCJwCENEH"
-                 })
-             ).json()
-            print(
-                f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: {response}")
-            return
-        except Exception as e:
-            err_time += 1
-            err_msg += f"err-{err_time}: {e}\n"
-            continue
-    print(f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: 请求失败")
-    raise Exception(f"请求失败:\n{err_msg}")
 
-def new_api(header, args):
-    err_time = 0
-    err_msg = ""
-    while err_time < 5:
-        try:
-            response = httpx.get(
-                f"https://cat-match.easygame2021.com/sheep/v1/game/user_rank_info?rank_score={args.rank_score}&rank_state={args.rank_state}&rank_time={args.rank_time}&rank_role={args.rank_role}&skin={args.skin}&uid={args.uid}", headers=header).json()
-            print(
-                f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: {response}")
-            return
-        except Exception as e:
-            err_time += 1
-            err_msg += f"err-{err_time}: {e}\n"
-            continue
-    print(f"运行时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}: 请求失败")
-    raise Exception(f"请求失败:\n{err_msg}")
+def map_api(header={}, log: bool = True):
+    map = api_get(
+        f"https://cat-match.easygame2021.com/sheep/v1/game/map_info_ex?matchType=3", header, False)
+    map = f"https://cat-match-static.easygame2021.com/maps/{map['data']['map_md5'][1]}.txt"
+    if log:
+        print(map)
+    header = dict(header)
+    header['Host'] = "cat-match-static.easygame2021.com"
+    map = api_get(map, header, log)['levelData']
+    count = sum([len(i) for i in list(map.values())])
+    # need to gen a step list (but i don't now how to impl)
+    return count
+
+
+def new_api(url="/sheep/v1/game/user_rank_info", header={}, args={}, log: bool = True):
+    api_get(
+        f"https://cat-match.easygame2021.com{url}?rank_score={args.rank_score}&rank_state={args.rank_state}&rank_time={args.rank_time}&rank_role={args.rank_role}&skin={args.skin}&uid={args.uid}",
+        header, log)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -94,14 +118,31 @@ def main():
 
     header = {
         'Host': 'cat-match.easygame2021.com',
-        'Connection': 'keep-alive',
         't': args.t,
         'content-type': 'application/json',
         'Accept-Encoding': 'gzip,compress,br,deflate',
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.28(0x18001c26) NetType/WIFI Language/zh_CN',
         'Referer': 'https://servicewechat.com/wx141bfb9b73c970a9/15/page-frame.html',
     }
-    new_api(header, args)
+
+    # 用户信息
+    # new_api("/sheep/v1/game/personal_info", header, args)
+
+    # 每日
+    map_api(header, log=False)
+    # print(f"will sleep {args.rank_time}s")
+    time.sleep(args.rank_time)
+    temp_api(header, args, log=False)
+    # for i in range(1, 26):
+    #     api_get(
+    #         f"https://cat-match.easygame2021.com/sheep/v1/game/update_user_skin?skin={i}", header)
+
+    # 用户信息
+    new_api("/sheep/v1/game/personal_info", header, args)
+    # new_api("/sheep/v1/game/user_rank_info", header, args)
+
+    # 话题
+    # new_api("/sheep/v1/game/topic_game_over", header, args)
 
 
 if __name__ == '__main__':
