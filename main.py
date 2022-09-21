9492f0d980e00f8bc11ae4b9ed64da0b843d3690
谢永诚 <752106133@qq.com>
2022-09-21 12:38:14 +0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import argparse
+import base64
 import json
-from re import T
+import random
+import struct
 import time
 import httpx
 import config
 
-http = httpx.Client(proxies={
-    'http://': 'http://127.0.0.1:8888',
-    'https://': 'http://127.0.0.1:8888'
-}, verify=False)
+http = httpx.Client()
 
 
 def api_get(url: str, header={}, log: bool = True):
@@ -82,6 +81,47 @@ def map_api(header={}, log: bool = True):
     return count
 
 
+def multi_byte_int(number: int):
+    # <128      : 1
+    # <16384    : 2
+    # <2097152  : 3
+    # <268435456: 4
+    # else      : 5
+    if number < 128:
+        return struct.pack('B', number)
+    elif number < 16384:
+        return struct.pack('BB', number & 127 | 128, number >> 7)
+    elif number < 2097152:
+        return struct.pack('BBB', number & 127 | 128, number >> 7 & 127 | 128, number >> 14)
+    elif number < 268435456:
+        return struct.pack('BBBB', number & 127 | 128, number >> 7 & 127 | 128, number >> 14 & 127 | 128, number >> 21)
+    else:
+        return struct.pack('BBBBB', number & 127 | 128, number >> 7 & 127 | 128, number >> 14 & 127 | 128, number >> 21 & 127 | 128, number >> 28)
+
+
+def gen_play_info(count: int, log: bool = True):
+    p = []
+    for h in range(count):  # 生成操作序列
+        p.append({'chessIndex': h,
+                 'timeTag': random.randint(0, 200)})
+    p[0]['timeTag'] = 0
+    if log:
+        print(len(p))
+    GAME_DAILY = 3
+    GAME_TOPIC = 4
+    data = struct.pack('BB', 8, GAME_DAILY)
+    for i in p:
+        c, t = i.values()
+        data += struct.pack('BBB', 34, 4, 8)
+        data += multi_byte_int(c)
+        data += struct.pack('B', 16)
+        data += multi_byte_int(t)
+    data = base64.b64encode(data).decode("utf-8")
+    if log:
+        print(data)
+    return data
+
+
 def new_api(url="/sheep/v1/game/user_rank_info", header={}, args={}, log: bool = True):
     api_get(
         f"https://cat-match.easygame2021.com{url}?rank_score={args.rank_score}&rank_state={args.rank_state}&rank_time={args.rank_time}&rank_role={args.rank_role}&skin={args.skin}&uid={args.uid}",
@@ -129,13 +169,15 @@ def main():
     # new_api("/sheep/v1/game/personal_info", header, args)
 
     # 每日
-    map_api(header, log=False)
     # print(f"will sleep {args.rank_time}s")
+    config.steps = gen_play_info(
+        map_api(header, log=False),
+        log=False)
     time.sleep(args.rank_time)
     temp_api(header, args, log=False)
-    # for i in range(1, 26):
-    #     api_get(
-    #         f"https://cat-match.easygame2021.com/sheep/v1/game/update_user_skin?skin={i}", header)
+    time.sleep(1)
+    # api_get(
+    #     f"https://cat-match.easygame2021.com/sheep/v1/game/update_user_skin?skin={24}", header, log=False)
 
     # 用户信息
     new_api("/sheep/v1/game/personal_info", header, args)
